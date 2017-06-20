0f0a5b755f70b216fc0a4341917c05f4caf393eb
WeltPixel Support <support@weltpixel.com>
2017-06-20 12:15:06 +0300

    applied ECE patch

---
+++ pearl_cloud_setups.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#usage ./pearl_cloud_setups.sh
+
+rm -rf  app/code/WeltPixel/CategoryPage/view/frontend/web/css/
+rm -rf  app/code/WeltPixel/CustomHeader/view/frontend/web/css/
+
+cd app/code/WeltPixel/CategoryPage/view/frontend/web/
+ln -s ../../../../../../../pub/media/css_weltpixel/CategoryPage/css/ css
+
+cd ../../../../../../../
+cd app/code/WeltPixel/CustomHeader/view/frontend/web/
+ln -s ../../../../../../../pub/media/css_weltpixel/CustomHeader/css/ css
+
+cd ../../../../../../../
+cd app/code/WeltPixel/FrontendOptions/view/frontend/
+mkdir web
+cd web
+ln -s ../../../../../../../pub/media/css_weltpixel/FrontendOptions/css/ css
+
+cd ../../../../../../../
+cd app/code/WeltPixel/ProductPage/view/frontend/web/
+ln -s ../../../../../../../pub/media/css_weltpixel/ProductPage/css/ css
+
+cd ../../../../../../../
+cd app/code/WeltPixel/QuickCart/view/frontend/web/
+ln -s ../../../../../../../pub/media/css_weltpixel/QuickCart/css/ css
+
+echo "DONE";
