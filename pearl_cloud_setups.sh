0620a3bfa2b4d5d0c349d7f3c5cc37a571c7b67a
WeltPixel Support <support@weltpixel.com>
2017-06-20 12:40:27 +0300

    applied ECE patch second step

--- pearl_cloud_setups.sh
+++ pearl_cloud_setups.sh
@@ -4,6 +4,10 @@
 
 rm -rf  app/code/WeltPixel/CategoryPage/view/frontend/web/css/
 rm -rf  app/code/WeltPixel/CustomHeader/view/frontend/web/css/
+rm -rf  app/code/WeltPixel/FrontendOptions/view/frontend/web/css/
+rm -rf  app/code/WeltPixel/ProductPage/view/frontend/web/css/
+rm -rf  app/code/WeltPixel/QuickCart/view/frontend/web/css/
+
 
 cd app/code/WeltPixel/CategoryPage/view/frontend/web/
 ln -s ../../../../../../../pub/media/css_weltpixel/CategoryPage/css/ css
@@ -13,9 +17,7 @@ cd app/code/WeltPixel/CustomHeader/view/frontend/web/
 ln -s ../../../../../../../pub/media/css_weltpixel/CustomHeader/css/ css
 
 cd ../../../../../../../
-cd app/code/WeltPixel/FrontendOptions/view/frontend/
-mkdir web
-cd web
+cd app/code/WeltPixel/FrontendOptions/view/frontend/web/
 ln -s ../../../../../../../pub/media/css_weltpixel/FrontendOptions/css/ css
 
 cd ../../../../../../../
